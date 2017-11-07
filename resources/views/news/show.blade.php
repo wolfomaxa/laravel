069784f7b5c0803b07a2e7072e74615b477e75e1
wolfomaxa <wolfomaxa@list.ru>
2017-11-07 12:14:58 +0200

    Update show.blade.php

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <article>
         <h1>{{ $news->title }}</h1>
-        <p>Pub on {{ $news->date->format('d.m.Y H:i:s')}}</p>
+        <p>Pub on {{ $news->date}}</p>
         <p>{!!   $news->bodytext !!}</p>
         <p>{!!   $news->replace_bodytext !!}</p>
 
